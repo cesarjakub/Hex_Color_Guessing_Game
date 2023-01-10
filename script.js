89373092920c90f64591dce7bc5a629120fe3682
cesarjakub <list>
2023-01-10 19:46:44 +0100

    js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const hexColor = document.querySelector('.hex');
+let hexColor = document.querySelector('.hex');
+let circleColor = document.querySelector('.color');
+
+const difftwo = document.querySelector('.diff2');
+const diffthree = document.querySelector('.diff3');
+const difffour = document.querySelector('.diff4');
 
 function correctHexColor(){
     let randomHex = Math.floor(Math.random()*16777215).toString(16).toUpperCase();
@@ -7,3 +12,19 @@ function correctHexColor(){
     return randomColor;
 }
 correctHexColor();
+
+function diff(pocet){
+    const d = document.createElement('div');
+    d.className = "circ";
+    for(let i = 0; i < pocet; i++){
+        circleColor.appendChild(d);
+    }
+}
+difftwo.addEventListener("click", diff(2));
+diffthree.addEventListener("click", diff(4));
+difffour.addEventListener("click", diff(5));
+
+
+function randomCircleColor(){
+
+}
